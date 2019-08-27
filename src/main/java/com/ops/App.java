52d0e185646c0d61cd7b771294de47ad67567c3e
tong'zhu'he <tongzhuhe@lidig.com>
2019-08-27 15:25:57 +0800

    tzh

--- src/main/java/com/ops/App.java
+++ src/main/java/com/ops/App.java
@@ -1,11 +1,13 @@
 package com.ops;
 
+import ops.model.X.area.EnableArea;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.ops.dao")
+@EnableArea
 public class App {
 
     public static void main(String[] args) {

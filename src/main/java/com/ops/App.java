0c13018a155bdc8af3a8ef9d116275925b3bf608
tong'zhu'he <tongzhuhe@lidig.com>
2019-09-10 11:12:45 +0800

    tzh

--- src/main/java/com/ops/App.java
+++ src/main/java/com/ops/App.java
@@ -5,9 +5,11 @@ import ops.model.X.file.system.EnableFileSystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @MapperScan("com.ops.dao")
+@EntityScan("com.ops.entity")
 @EnableArea
 @EnableFileSystem
 public class App {
